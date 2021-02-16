ef8955d735e78bfb41e02f0bb37f911978aaa6ca
Julien Deleligne <julien.deleligne@decathlon.com>
2021-02-16 09:26:33 +0100

    First Hello World

--- src/main/java/com/openclassrooms/safetyNetAlerts/service/PrintInConsole.java
+++ src/main/java/com/openclassrooms/safetyNetAlerts/service/PrintInConsole.java
@@ -1,7 +1,9 @@
 package com.openclassrooms.safetyNetAlerts.service;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PrintInConsole implements CommandLineRunner {
 
   @Override
