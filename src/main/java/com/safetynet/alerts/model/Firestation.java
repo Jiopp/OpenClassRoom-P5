e2455997ed52d3e9180b98d2711b93b39ed6eefc
Julien Deleligne <julien.deleligne@decathlon.com>
2021-02-18 17:16:32 +0100

    fix issue with databse

--- src/main/java/com/safetynet/alerts/model/Firestation.java
+++ src/main/java/com/safetynet/alerts/model/Firestation.java
@@ -14,7 +14,7 @@ import lombok.Data;
 public class Firestation {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   @Column
